fe35bc0230a9a462a05cfa21d09ff55e787ea0e0
fyurchik bc2024-6 <jyrafenun@gmail.com>
2024-11-24 12:00:45 +0200

    first

---
+++ index.js
@@ -0,0 +1,110 @@
+const fs = require("fs").promises;
+const http = require("http");
+const { Command } = require("commander");
+const path = require("path");
+const superagent = require("superagent");
+
+const program = new Command();
+
+program
+  .requiredOption("-h, --host <host>", "адреса сервера")
+  .requiredOption("-p, --port <port>", "порт сервера")
+  .requiredOption(
+    "-c, --cache <path>",
+    "шлях до директорії, яка міститиме закешовані файли"
+  );
+
+program.parse(process.argv);
+
+const { host, port, cache } = program.opts();
+
+function getCacheFilePath(code) {
+  return path.join(cache, `${code}.jpg`);
+}
+
+const fetchFromHttpCat = async (httpCode) => {
+  try {
+    const response = await superagent.get(`https://http.cat/${httpCode}`);
+    return response.body;
+  } catch (err) {
+    throw new Error("Картинка не знайдена на сервері http.cat");
+  }
+};
+
+const server = http.createServer(async (req, res) => {
+  const urlParts = req.url.split("/");
+  const httpCode = urlParts[1];
+
+  if (!httpCode) {
+    res.writeHead(400, { "Content-Type": "text/plain" });
+    res.end("Невірний запит");
+    return;
+  }
+
+  const cacheFilePath = getCacheFilePath(httpCode);
+
+  switch (req.method) {
+    case "GET": {
+      try {
+        const imageData = await fs.readFile(cacheFilePath);
+        res.writeHead(200, { "Content-Type": "image/jpeg" });
+        console.log("взято з кешу");
+        res.end(imageData);
+      } catch (err) {
+        const httpCatImage = await fetchFromHttpCat(httpCode);
+
+        if (httpCatImage) {
+          console.log("взято з сайту");
+          await fs.writeFile(cacheFilePath, httpCatImage);
+          res.writeHead(200, { "Content-Type": "image/jpeg" });
+          res.end(httpCatImage);
+        } else {
+          res.writeHead(404, { "Content-Type": "text/plain" });
+          res.end("картинка не знайдена на http.cat");
+        }
+      }
+      break;
+    }
+
+    case "PUT": {
+      let imageData = [];
+
+      req.on("data", (chunk) => {
+        imageData.push(chunk);
+      });
+      req.on("end", async () => {
+        imageData = Buffer.concat(imageData);
+        try {
+          await fs.writeFile(cacheFilePath, imageData);
+          res.writeHead(201, { "Content-Type": "text/plain" });
+          res.end("Картинка збережена");
+        } catch (err) {
+          res.writeHead(500, { "Content-Type": "text/plain" });
+          res.end("Помилка при записі файлу");
+        }
+      });
+      break;
+    }
+
+    case "DELETE": {
+      try {
+        await fs.unlink(cacheFilePath);
+        res.writeHead(200, { "Content-Type": "text/plain" });
+        res.end("Картинка видалена");
+      } catch (err) {
+        res.writeHead(404, { "Content-Type": "text/plain" });
+        res.end("Картинка не знайдена");
+      }
+      break;
+    }
+
+    default: {
+      res.writeHead(405, { "Content-Type": "text/plain" });
+      res.end("Метод не дозволено");
+      break;
+    }
+  }
+});
+server.listen(port, host, () => {
+  console.log(`Сервер запущено на ${host}:${port}`);
+});
